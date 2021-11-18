ff015215de47d9121a57db19c3627b9080754943
XDwan <xdwanshijie@outlook.com>
2021-11-18 22:53:44 +0800

    Java&weiapp init

---
+++ Java/src/main/java/com/example/deadlinereminder/DeadlineReminderApplication.java
@@ -0,0 +1,13 @@
+package com.example.deadlinereminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeadlineReminderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeadlineReminderApplication.class, args);
+    }
+
+}
---
+++ Java/src/test/java/com/example/deadlinereminder/DeadlineReminderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deadlinereminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeadlineReminderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
